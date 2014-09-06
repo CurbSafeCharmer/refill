e8ba0c2311298cfff6ec0f5f7f8b7fbff220791c
Zhaofeng Li <hello@zhaofeng.li>
2014-09-07 07:04:08 +0800

    Use textarea for result for easy copying

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ Not a bare reference: <ref>[http://example.com not a bare ref]</ref></textarea>
 			$text = fixRef( $_POST['text'] );
 		}
 		$stext = htmlspecialchars( $text );
-		echo "<h2>Result</h2><pre id='result'>$stext</pre>";
+		echo "<h2>Result</h2><textarea id='result' rows='100' cols='10'>$stext</textarea>";
 	?>
 	<small><a href="https://github.com/zhaofengli/reflinks">Source</a> ♦ <a href="https://en.wikipedia.org/wiki/User:Zhaofeng_Li">Zhaofeng Li</a> ♦ Original Reflinks by Dispenser</small>
 </body>
