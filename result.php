ae7bd0bb9dfef66394e92ba6242f8f7a8d1381dc
Zhaofeng Li <hello@zhaofeng.li>
2014-09-07 21:27:30 +0800

    Footer tweaks

--- result.php
+++ result.php
@@ -45,7 +45,7 @@ $utitle = urlencode( $title );
 		echo "</form>";
 	?>
 	<footer>
-		<a href="https://github.com/zhaofengli/reflinks">Source</a> ♦ <a href="https://en.wikipedia.org/wiki/User:Zhaofeng_Li">Zhaofeng Li</a> ♦ Original Reflinks by Dispenser
+		<a href="https://github.com/zhaofengli/reflinks">Source</a> ♦ <a href="https://en.wikipedia.org/wiki/User:Zhaofeng_Li/Reflinks">Info</a> ♦ by <a href="https://en.wikipedia.org/wiki/User:Zhaofeng_Li">Zhaofeng Li</a> ♦ Original Reflinks by <a href="https://en.wikipedia.org/wiki/User:Dispenser">Dispenser</a>
 	</footer>
 </body>
 </html>
