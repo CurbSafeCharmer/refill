233adbc8e78a8509f3ec7d555553dffaa7063472
Zhaofeng Li <hello@zhaofeng.li>
2014-09-09 10:01:17 +0000

    Hack to skip summary check

--- result.php
+++ result.php
@@ -83,6 +83,7 @@ $utitle = urlencode( $title );
 		}
 		echo "<textarea name='wpTextbox1' rows='10' cols='100'>$sresult</textarea>";
 		echo "<input type='hidden' name='wpSummary' value='{$config['summary']}'/>";
+		echo "<input type='hidden' name='wpAutoSummary' value='y'/>";
 		echo "<input type='hidden' name='wpStarttime' value='$timestamp'/>";
 		if ( !empty( $title ) && count ( $log['fixed'] ) ) {
 			echo "<input type='submit' name='wpPreview' value='Preview / Save on wiki'/>";
