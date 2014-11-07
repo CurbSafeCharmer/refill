ae5670675dd946dc97c138acabf692589d8097b9
Zhaofeng Li <hello@zhaofeng.li>
2014-11-07 15:11:20 +0000

    footer.php: Allow extending the footer with config/footer.php

--- src/footer.php
+++ src/footer.php
@@ -1,3 +1,6 @@
 <footer>
 	<a href="https://en.wikipedia.org/wiki/User:Zhaofeng_Li/Reflinks">Manual</a> ♦ <a href="https://github.com/zhaofengli/reflinks">Source</a> ♦ by <a href="https://en.wikipedia.org/wiki/User:Zhaofeng_Li">Zhaofeng Li</a> ♦ Original Reflinks by <a href="https://en.wikipedia.org/wiki/User:Dispenser">Dispenser</a>
+<?php
+	@include __DIR__ . "/../config/footer.php";
+?>
 </footer>
