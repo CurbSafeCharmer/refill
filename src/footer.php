cda4d79da064ecb4788cb58859380a91fbc5bb87
Zhaofeng Li <hello@zhaofeng.li>
2014-12-06 14:46:17 +0000

    Use <ul> for the footer link list

--- src/footer.php
+++ src/footer.php
@@ -1,6 +1,11 @@
 <footer>
-	<a href="https://en.wikipedia.org/wiki/User:Zhaofeng_Li/Reflinks">Manual</a> ♦ <a href="https://github.com/zhaofengli/reflinks">Source</a> ♦ by <a href="https://en.wikipedia.org/wiki/User:Zhaofeng_Li">Zhaofeng Li</a> ♦ Original Reflinks by <a href="https://en.wikipedia.org/wiki/User:Dispenser">Dispenser</a>
+	<ul id="footerlinks">
+		<li><a href="https://en.wikipedia.org/wiki/User:Zhaofeng_Li/Reflinks">Manual</a></li>
+		<li><a href="https://github.com/zhaofengli/reflinks">Source</a></li>
+		<li>by <a href="https://en.wikipedia.org/wiki/User:Zhaofeng_Li">Zhaofeng Li</a></li>
+		<li>Original Reflinks by <a href="https://en.wikipedia.org/wiki/User:Dispenser">Dispenser</a></li>
 <?php
 	@include __DIR__ . "/../config/footer.php";
 ?>
+	</ul>
 </footer>
