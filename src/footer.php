301db0276fbf4826bac14ffb32c7303a7b1ef358
Zhaofeng Li <hello@zhaofeng.li>
2014-12-31 23:01:45 +0800

    footer.php: Remove reference to Dispenser - A more complete acknowledgement page on the way
    Signed-off-by: Zhaofeng Li <hello@zhaofeng.li>

--- src/footer.php
+++ src/footer.php
@@ -3,7 +3,6 @@
 		<li><a href="https://en.wikipedia.org/wiki/User:Zhaofeng_Li/Reflinks">Manual</a></li>
 		<li><a href="https://github.com/zhaofengli/reflinks">Source</a></li>
 		<li>by <a href="https://en.wikipedia.org/wiki/User:Zhaofeng_Li">Zhaofeng Li</a></li>
-		<li>Original Reflinks by <a href="https://en.wikipedia.org/wiki/User:Dispenser">Dispenser</a></li>
 <?php
 	@include __DIR__ . "/../config/footer.php";
 ?>
