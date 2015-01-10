e220e2dd30885a60e87f8e17fafdf5ece65035c3
Zhaofeng Li <hello@zhaofeng.li>
2015-01-10 18:27:39 +0800

    Forgot to add the new messages...
    Signed-off-by: Zhaofeng Li <hello@zhaofeng.li>

--- misc/Reflinks.i18n.php
+++ misc/Reflinks.i18n.php
@@ -24,6 +24,7 @@ $messages['en'] = array(
 	'heading-citegen' => 'Generate a reference',
 	'heading-result' => 'Result',
 	'heading-newwikitext' => 'New Wiki markup',
+	'heading-error' => 'Error',
 	
 	// Input placeholders
 	'placeholder-pagename' => 'Page name',
@@ -37,14 +38,29 @@ $messages['en'] = array(
 	'label-acknowledgements' => 'Acknowledgements',
 	'label-reportbugs' => 'Report bugs',
 	'label-switchlang' => 'Languages',
+	'label-save' => 'Preview / Save on Wiki',
+	
+	// General messages
+	'fixed' => '$1 reference(s) fixed!',
+	'nofixed' => 'No references fixed.',
+	'responsibility' => 'You are responsible for every edit you make.',
+	'pleasedoublecheck' => 'Please double-check the edit before saving!',
+	'noaccessdate' => 'Note: Dates of access are omitted in the result. Please verify whether the references still support the statements, and add the dates where appropriate.',
+	'chancetoreview' => 'You will have a chance to review the edit before it\'s saved.',
+	'colourlegend' => 'Colours:',
+	'colour-insert' => 'Blue',
+	'colour-delete' => 'Orange',
+	'diff-insert' => 'Added',
+	'diff-delete' => 'Removed',
+	
 	
 	// Misc.
 	'comingsoon' => 'Coming soon',
-	'responsibility' => 'You are responsible for every edit you make.',
-	'pleasedoublecheck' => 'Please double-check the edit before saving!',
+	'uhoh' => 'Uh-oh!',
+	'developedby' => 'by $1',
 );
 
-$messages['zh'] = array(
+$messages['zh-hans'] = array(
 	'appname' => '来源扩充',
 	'tagline' => '轻松填充裸露来源链接',
 	
@@ -55,6 +71,7 @@ $messages['zh'] = array(
 	'heading-citegen' => '生成来源',
 	'heading-result' => '结果',
 	'heading-newwikitext' => '新维基代码',
+	'heading-error' => '错误',
 	
 	// 输入提示
 	'placeholder-pagename' => '页面名称',
@@ -68,9 +85,24 @@ $messages['zh'] = array(
 	'label-acknowledgements' => '致谢',
 	'label-reportbugs' => '报告错误',
 	'label-switchlang' => '切换语言',
+	'label-save' => '在维基上预览/保存',
+	
+	// 一般信息
+	'fixed' => '扩展了 $1 个引证！',
+	'nofixed' => '没有修复引证。',
+	'responsibility' => '你对自己作出的每一个编辑负责。',
+	'pleasedoublecheck' => '请在保存前再三检查！',
+	'noaccessdate' => '注：在结果中没有添加存取日期。请确认提供的来源还能支持页面中的论述，并补充存取日期。',
+	'chancetoreview' => '你在保存前将有机会再次检查修改。',
+	'colourlegend' => '颜色：',
+	'colour-insert' => '蓝色',
+	'colour-delete' => '橙色',
+	'diff-insert' => '增加',
+	'diff-delete' => '删除',
+	
 	
 	// 其他
 	'comingsoon' => '敬请期待',
-	'responsibility' => '你对自己作出的每一个编辑负责。',
-	'pleasedoublecheck' => '请在保存前再三检查！',
+	'uhoh' => '噢，不！',
+	'developedby' => '由 $1 开发',
 );
