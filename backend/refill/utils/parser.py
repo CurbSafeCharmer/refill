aae5498ece23f88e304e246d30c5bfae192bb4de
Zhaofeng Li <hello@zhaofeng.li>
2019-01-13 18:24:47 -0800

    parser.py: Improve parsing logic

--- backend/refill/utils/parser.py
+++ backend/refill/utils/parser.py
@@ -2,6 +2,7 @@ import mwparserfromhell
 from mwparserfromhell.wikicode import Wikicode
 from mwparserfromhell.nodes.external_link import ExternalLink
 from mwparserfromhell.nodes.template import Template
+from mwparserfromhell.nodes.text import Text
 from ..models import Citation
 from . import Utils
 
@@ -26,7 +27,7 @@ class Parser:
             content = mwparserfromhell.parse(content)
 
         citation = Citation()
-        for node in content.ifilter(forcetype=(ExternalLink, Template), recursive=False):
+        for node in content.ifilter(recursive=False):
             if type(node) is ExternalLink:
                 citation.url = str(node.url)
                 if node.title:
@@ -37,9 +38,12 @@ class Parser:
                     if tname in Parser.TEMPLATE_TYPE_MAPPING:
                         citation.type = Parser.TEMPLATE_TYPE_MAPPING[tname]
 
-                    for field in Parser.TEMPLATE_FIELD_MAPPING:
-                        if node.has(field):
-                            citation[Parser.TEMPLATE_FIELD_MAPPING[field]] = str(node.get(field).value)
+                    for param in node.params:
+                        if param in Parser.TEMPLATE_FIELD_MAPPING:
+                            citation[Parser.TEMPLATE_FIELD_MAPPING[param]] = str(node.get(param).value)
+                        else:
+                            # Do not touch citations with unparsable data
+                            return False
                 elif tname == 'Webarchive':
                     if node.has('url'):
                         citation.archiveurl = str(node.get('url').value)
@@ -47,6 +51,13 @@ class Parser:
                         citation.archivedate = str(node.get('date').value)
                     if node.has('title'):
                         citation.title = str(node.get('title').value)
+            elif type(node) is Text:
+                if str(node).strip():
+                    # Non-empty text node
+                    return False
+            else:
+                # Do not touch citations with unparsable data
+                return False
 
         if citation.isLocatable():
             return citation
