fa9e289b240d44ef83a87cbd74bde1b8b65bb874
GlazerMann <kurt@ioiowa.com>
2023-06-04 17:22:13 -0500

    Hopefully fix tag bug

--- backend/refill/transforms/mergeref.py
+++ backend/refill/transforms/mergeref.py
@@ -60,6 +60,7 @@ class MergeRef(Transform):
             else:
                 name = names[0]
 
+            changed = []
             first = True
             for tag in tags:
                 if first:
@@ -67,7 +68,16 @@ class MergeRef(Transform):
                 else:
                     tag.self_closing = True
 
+                old_name = self._getName(tag)
                 self._setName(tag, name)
+                if old_name != name :
+                    changed.append(old_name)
+             
+            all_tags = [e[2] for e in collection]
+                for tag in all_tags:
+                    if (self._getName(tag) in changed):  # We just changed <ref name=X>stuff</ref>, now do <ref name=X/>
+                        self._setName(tag, name)
+
 
         self._ctx.reportProgress("SUCCESS", 1, {})
         return wikicode
