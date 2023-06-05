513a7d16915bd7a8e60512a956c384485c9df680
GlazerMann <kurt@ioiowa.com>
2023-06-05 14:46:16 -0500

    Update MergeRef.py

--- backend/refill/transforms/MergeRef.py
+++ backend/refill/transforms/MergeRef.py
@@ -70,7 +70,7 @@ class MergeRef(Transform):
 
                 old_name = self._getName(tag)
                 self._setName(tag, name)
-                if old_name != name :
+                if old_name != name:
                     changed.append(old_name)
 
             all_tags = [e[2] for e in collection]
