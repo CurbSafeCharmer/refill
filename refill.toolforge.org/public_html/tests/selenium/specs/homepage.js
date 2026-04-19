70345139f29ef4c7caca7922f17e593af5830603
NovemLinguae <novemlinguae@gmail.com>
2026-04-18 19:46:18 -0700

    add selenium test for language / internationalization

--- refill.toolforge.org/public_html/tests/selenium/specs/homepage.js
+++ refill.toolforge.org/public_html/tests/selenium/specs/homepage.js
@@ -1,9 +1,16 @@
 describe('Homepage', () => {
-  it('loads the Vue app', async () => {
+  it('loads the Vue app in English', async () => {
     // add the date as a param to prevent caching issues
     await browser.url('/?_=' + Date.now());
     const tagline = await $('h2.tagline');
     await tagline.waitForExist({ timeout: 5000 });
     await expect(tagline).toHaveTextContaining('Expand bare references with ease');
+  });
+  it('loads the Vue app in Spanish', async () => {
+    await browser.setCookies({ name: 'TsIntuition_userlang', value: 'es' });
+    await browser.url('/?_=' + Date.now());
+    const tagline = await $('h2.tagline');
+    await tagline.waitForExist({ timeout: 5000 });
+    await expect(tagline).toHaveTextContaining('Ampliar las referencias sencillas con facilidad');
   });
 });
